11214c9d1b6c12e01314646c19f8cc4baab128c0
officialMozart <68749045+officialMozart@users.noreply.github.com>
2021-07-12 15:40:09 +0200

    Add files via upload

--- sBroadcasts/src/main/java/com/github/sbroadcast/commandBroadcast.java
+++ sBroadcasts/src/main/java/com/github/sbroadcast/commandBroadcast.java
@@ -6,23 +6,36 @@ import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class commandBroadcast implements CommandExecutor {
+
+public class commandBroadcast extends JavaPlugin implements CommandExecutor, Listener {
+    FileConfiguration config = getConfig();
+    String prefix = config.getString("broadcastMessage");
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(args.length == 0) return false;
+
 
         String broadcastString = "";
 
         for(String loopValue : args) {
             broadcastString = broadcastString + loopValue + " ";
         }
+        if(config.getString("broadcastMessage") != "") {
+            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', prefix + broadcastString));
+            if(config.getBoolean("playSound")) {
+                for(Player loopPlayer : Bukkit.getOnlinePlayers()) {
+                    loopPlayer.getWorld().playSound(loopPlayer.getLocation(), Sound.ENTITY_GHAST_SCREAM, 4.0F, 1.0F);
+                }
+            } else {
+                Bukkit.getLogger().info("Broadcast command was used without sound");
+            }
 
-        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&e&lAlert &8&l»&7 " + broadcastString));
-
-        for(Player loopPlayer : Bukkit.getOnlinePlayers()) {
-            loopPlayer.getWorld().playSound(loopPlayer.getLocation(), Sound.ENTITY_GHAST_SCREAM, 3.0F, 1.0F);
         }
 
         return true;
--- sBroadcasts/src/main/java/com/github/sbroadcast/sBroadcast.java
+++ sBroadcasts/src/main/java/com/github/sbroadcast/sBroadcast.java
@@ -1,20 +1,30 @@
 package com.github.sbroadcast;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class sBroadcast extends JavaPlugin {
+public final class sBroadcast extends JavaPlugin implements Listener {
+    FileConfiguration config = getConfig();
 
     @Override
     public void onEnable() { // Startup Things
 
-        Bukkit.getLogger().info("hey");
+        //config stuff
+        config.addDefault("broadcastMessage", "&e&lAlert &8&l»&7 ");
+        config.addDefault("playSound", true);
+        config.options().copyDefaults(true);
+        saveConfig();
+        //config stuff
+
+        Bukkit.getLogger().info("S Broadcasts Loaded");
         this.getCommand("broadcast").setExecutor(new commandBroadcast());
     }
 
     @Override
     public void onDisable() { // Shutdown Stuff
 
-        Bukkit.getLogger().info("bye");
+        Bukkit.getLogger().info("S Broadcasts Unloaded");
     }
 
 }
